74a16e87869238b0a22f8e6ee0ca757e9a84f595
kseravin <kseravin@freeconferencecall.com>
2024-05-21 11:32:21 +0300

    refactor data storage module

--- async_api/src/db/data_storage.py
+++ async_api/src/db/data_storage.py
@@ -1,8 +1,8 @@
-from abc import ABC, abstractmethod
 from typing import Any, Dict, List
 import logging
-from enum import Enum
+from uuid import UUID
 
+from pydantic import BaseModel
 from elasticsearch import AsyncElasticsearch, ConnectionError
 
 from backoff import backoff
@@ -10,115 +10,61 @@ from backoff import backoff
 logger = logging.getLogger(__name__)
 
 
-class DataStorageEntity(str, Enum):
-    PERSON = 'person'
-    FILM = 'film'
-    GENRE = 'genre'
-
-
 class DataStorageError(Exception):
     pass
 
 
-class AbstractDataStorage(ABC):
-    @abstractmethod
-    async def get(
-        self,
-        entity: DataStorageEntity,
-        limit: int = 50,
-        offset: int = 0,
-        sort_by: str = '',
-        **query_params
-    ) -> List[Dict[str, Any]]:
-        pass
-
-    @abstractmethod
-    async def search(
-        self,
-        entity: DataStorageEntity,
-        query: str,
-        limit: int = 50,
-        offset: int = 0
-    ) -> List[Dict[str, Any]]:
-        pass
+class Filters(BaseModel):
+    pass
 
 
-class ElasticDataStorage(AbstractDataStorage):
-    _STORAGE_ENTITY_INDEX_MAP = {
-        DataStorageEntity.PERSON: 'personas',
-        DataStorageEntity.FILM: 'movies',
-        DataStorageEntity.GENRE: 'genres',
-    }
+class FilmFilters(Filters):
+    genre_id: UUID | None
 
-    def __init__(self, elastic: AsyncElasticsearch):
+
+class DataStorage:
+    def __init__(self, elastic: AsyncElasticsearch, index: str):
         self._elastic = elastic
+        self._index = index
 
-    async def get(
-        self,
-        entity: DataStorageEntity,
-        limit: int = 50,
-        offset: int = 0,
-        sort_by: str = 'id',
-        **query_params
+    async def get(self, id: UUID) -> Dict[str, Any] | None:
+        logger.info('Getting %s by id %s', self._index, id)
+        query_body = {'query': {'match': {'id': {'query': id}}}}
+        films = await self._make_request(query_body)
+        return films[0] if films else None
+
+    async def list(
+        self, limit: int = 50, offset: int = 0, sort_by: str = 'id', filters: Filters | None = None
     ) -> List[Dict[str, Any]]:
-        logger.info('Getting %s with query params: %s', entity, query_params)
         query_body = {
+            **self._get_sort_field(sort_by),
             **self._get_elastic_pagination_fields(limit, offset),
+            **self._apply_filters(filters),
         }
-        if sort_by:
-            if sort_by.startswith('-'):
-                sort_by = sort_by[1:]
-                sort_direction = 'desc'
-            else:
-                sort_direction = 'asc'
-            query_body['sort'] = {sort_by: sort_direction}
-
-        if query_params:
-            # consider just one for now
-            param, value = next(iter(query_params.items()))
-            if value:
-                query_body['query'] = {}
-                if len(param.split('__')) == 2:
-                    nested_path, nested_field = param.split('__')
-                    query_body['query']['nested'] = {
-                        'path': nested_path,
-                        'query': {'match': {f'{nested_path}.{nested_field}': {'query': value}}},
-                    }
-                else:
-                    query_body['query']['match'] = {param: {'query': value}}
-
-        return await self._make_request(entity, query_body)
+        return await self._make_request(query_body)
 
     async def search(
-        self,
-        entity: DataStorageEntity,
-        query: str,
-        limit: int = 50,
-        offset: int = 0
+        self, query: str, limit: int = 50, offset: int = 0
     ) -> List[Dict[str, Any]]:
-        logger.info('Searching %s by query: %s', entity, query)
+        logger.info('Searching in %s by query: %s', self._index, query)
         query_body = {
             'query': {'query_string': {'query': query}},
             **self._get_elastic_pagination_fields(limit, offset),
         }
-        return await self._make_request(entity, query_body)
+        return await self._make_request(query_body)
 
-    async def _make_request(self, entity: DataStorageEntity, query_body: Dict[str, Any]) -> List[Dict[str, Any]]:
-        logger.info('Requesting %s with query body: %s', entity, query_body)
-        index = self._get_index_by_entity(entity)
+    async def _make_request(self, query_body: Dict[str, Any]) -> List[Dict[str, Any]]:
+        logger.info('Requesting %s with query body: %s', self._index, query_body)
         try:
-            response = await self._make_search_request(index, query_body)
+            response = await self._make_search_request(query_body)
         except ConnectionError as e:
-            logger.error('Failed to request %s with query body: %s', entity, query_body)
+            logger.error('Failed to request %s with query body: %s', self._index, query_body)
             raise DataStorageError(e)
         return [f['_source'] for f in ((response.get('hits') or {}).get('hits') or [])]
 
     @backoff(exceptions=(ConnectionError,))
-    async def _make_search_request(self, index: str, query_body: Dict[str, Any]) -> Dict[str, Any]:
-        return await self._elastic.search(index=index, body=query_body)
-
-    def _get_index_by_entity(self, entity: DataStorageEntity) -> str:
-        return self._STORAGE_ENTITY_INDEX_MAP[entity]
+    async def _make_search_request(self, query_body: Dict[str, Any]) -> Dict[str, Any]:
+        return await self._elastic.search(index=self._index, body=query_body)
 
     @staticmethod
     def _get_elastic_pagination_fields(limit: int, offset: int) -> Dict[str, int]:
@@ -126,3 +72,32 @@ class ElasticDataStorage(AbstractDataStorage):
             'from': offset,
             'size': limit,
         }
+
+    @staticmethod
+    def _get_sort_field(sort_by: str) -> Dict[str, Any]:
+        if sort_by.startswith('-'):
+            sort_by = sort_by[1:]
+            sort_direction = 'desc'
+        else:
+            sort_direction = 'asc'
+        return {
+            'sort': {sort_by: sort_direction},
+        }
+
+    @staticmethod
+    def _apply_filters(filters: Filters | None) -> Dict[str, Any]:
+        return {}
+
+
+class FilmDataStorage(DataStorage):
+    @staticmethod
+    def _apply_filters(filters: FilmFilters | None) -> Dict[str, Any]:
+        if not filters:
+            return {}
+        if filters.genre_id:
+            return {
+                'query': {
+                    'nested': {
+                        'path': 'genres',
+                        'query': {'match': {'genres.id': {'query': filters.genre_id}}}}}}
+        return {}
